7e6ca595d4d9780a4b9582d2174a40e913c0ca61
keshav.kashyap <keshav.kashyap@tothenew.com>
2018-09-11 17:45:48 +0530

    Added Login and Post API's

--- specs/post_test_spec.js
+++ specs/post_test_spec.js
@@ -14,19 +14,18 @@ var util = require('util'),
 
 describe('Test GraphQL SCHEDULE API queries', function () {
 
-    var chefDaySchedule,chefWeeklySchedule,createDaySchedule,createWeeklySchedule,updateDaySchedule,updateWeeklySchedule;
-
+    var createPost,deletePost,featuredPosts,getPostById,posts,updatePost;
 
     beforeEach(function (done) {
         if (!userInfo) {
             helperUtil.envInfo();
 
-            chefDaySchedule = "query {chefDaySchedule(chefId: \"64f3c481-0129-4af4-b35f-c9e9cf53f042\", date: \"2018-08-10\") { date available slots{start end} }}";
-            chefWeeklySchedule = "query {chefSchedule(chefId: \"4c686360-f6ef-4dbc-9e9c-7b70a0f82ebe\") { daySchedules{ day slots{start end} } }}";
-            createDaySchedule = "mutation { createDaySchedule(chefId: \"faa4d99e-301e-473e-ad71-99d8b0636868\", date: \"2018-08-10\", dateSchedule: { date: \"2018-08-13\", available: true, slots: [{ start: \"10:00\", end: \"15:00\" }] }) }";
-            createWeeklySchedule = "mutation { createSchedule(chefId: \"4c686360-f6ef-4dbc-9e9c-7b70a0f82ebe\", weekschedule: { daySchedules: [{day: MONDAY, slots: [{ start: \"10:00\", end: \"13:00\" }, { start: \"15:00\", end: \"17:00\" }, { start: \"18:00\", end: \"20:00\" }]}, {day: TUESDAY, slots: [{ start: \"10:00\", end: \"13:00\" }, { start: \"15:00\", end: \"17:00\" }, { start: \"18:00\", end: \"20:00\" }]}, {day: WEDNESDAY, slots: [{ start: \"10:00\", end: \"13:00\" }, { start: \"15:00\", end: \"17:00\" }, { start: \"18:00\", end: \"20:00\" }]} ] }) }";
-            updateDaySchedule = "mutation { updateDaySchedule(chefId: \"4c686360-f6ef-4dbc-9e9c-7b70a0f82ebe\", date: \"2018-08-10\", dateSchedule: { date: \"2018-08-10\", available: true, slots: [{ start: \"10:00\", end: \"14:30\" }] }) }";
-            updateWeeklySchedule = "mutation { updateSchedule(chefId: \"4c686360-f6ef-4dbc-9e9c-7b70a0f82ebe\", weekschedule: { daySchedules: [{day: MONDAY, slots: [{ start: \"10:00\", end: \"13:00\" }, { start: \"15:00\", end: \"17:00\" }, { start: \"18:00\", end: \"20:00\" }]}, {day: TUESDAY, slots: [{ start: \"10:00\", end: \"13:00\" }, { start: \"15:00\", end: \"17:00\" }, { start: \"18:00\", end: \"20:00\" }]}, {day: THURSDAY, slots: [{ start: \"11:00\", end: \"13:00\" }, { start: \"15:00\", end: \"17:00\" }, { start: \"18:00\", end: \"20:00\" }]} ] }) }";
+            createPost = "mutation { createPost( post: { chefId : \"4c686360-f6ef-4dbc-9e9c-7b70a0f82ebe\", title : \"Fungee1\", body : \"Laborum ad occaecat dolore fugiat id. Lorem officia irure mollit adipisicing laborum voluptate exercitation voluptate fugiat in proident. Culpa anim laboris nulla id reprehenderit esse cillum voluptate consequat quis. Laborum incididunt voluptate reprehenderit sunt sit sunt aliqua in minim elit.\",  tags: [ \"Fungee\" , \"Wiener Schnitzel\", \"Bermuda fish chowder\" ], media : [ { type : VIDEO, url : \"https://unsplash.com/photos/Gg5-K-mJwuQ\" } ], isDraft:false }) }";
+            deletePost = "mutation { deletePost(id: \"9c3e90c2-934a-409b-94d0-2d7f0c5d6c3e\") }";
+            featuredPosts = "query {featuredPosts(postCount: 10) {id chefId title blurb body isDraft tags numOfLikes media{ type url }}}";
+            getPostById = "query {post(id: \"59d5e543-296e-4432-a42e-02303f676f8c\") {id chefId title blurb body isDraft tags numOfLikes media{ type url } } }";
+            posts = "query {posts(filters: { title: \"furnance\", isDraft: false, tags:[\"Chinese\"] }, cursor: null, pageSize: 6)} { posts{id chefId title blurb body isDraft tags numOfLikes media{ type url }} endCursor hasMore }";
+            updatePost = "mutation { updatePost( post: { id: \"9c3e90c2-934a-409b-94d0-2d7f0c5d6c3e\", title : \"Fungee1\", body : \"Laborum ad occaecat dolore fugiat id. Lorem officia irure mollit adipisicing laborum voluptate exercitation voluptate fugiat in proident. Culpa anim laboris nulla id reprehenderit esse cillum voluptate consequat quis. Laborum incididunt voluptate reprehenderit sunt sit sunt aliqua in minim elit.\",  tags: [ \"Fungee\" , \"Wiener Schnitzel\", \"Bermuda fish chowder\" ], isDraft:false }) }";
 
             done();
 
@@ -35,14 +34,14 @@ describe('Test GraphQL SCHEDULE API queries', function () {
         }
     });
 
-    it('ZESTY_SCHEDULE-001 :Chef Day Schedule api', function (done) {
+    it('ZESTY_POST-001 :Create Post api', function (done) {
 
-        helperUtil.addStep("Request Payload :: "+chefDaySchedule);
+        helperUtil.addStep("Request Payload :: "+createPost);
 
         fetch(JSONData.AutoTextList[0].BASE_URL + JSONData.AutoTextList[0].REDIRECT_URL, {
             method: 'POST',
             headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + global.authToken},
-            body: JSON.stringify({query: chefDaySchedule}),
+            body: JSON.stringify({query: createPost}),
         }).then(function (res) {
 
             return res.json();
@@ -53,14 +52,14 @@ describe('Test GraphQL SCHEDULE API queries', function () {
         });
     });
 
-    it('ZESTY_SCHEDULE-002 :Chef Weekly Schedule api', function (done) {
+    it('ZESTY_POST-002 :Delete Post api', function (done) {
 
-        helperUtil.addStep("Request Payload :: "+chefWeeklySchedule);
+        helperUtil.addStep("Request Payload :: "+deletePost);
 
         fetch(JSONData.AutoTextList[0].BASE_URL + JSONData.AutoTextList[0].REDIRECT_URL, {
             method: 'POST',
             headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + global.authToken},
-            body: JSON.stringify({query: chefWeeklySchedule}),
+            body: JSON.stringify({query: deletePost}),
         }).then(function (res) {
 
             return res.json();
@@ -71,14 +70,14 @@ describe('Test GraphQL SCHEDULE API queries', function () {
         });
     });
 
-    it('ZESTY_SCHEDULE-003 :Create Day Schedule api', function (done) {
+    it('ZESTY_POST-003 :Featured Post api', function (done) {
 
-        helperUtil.addStep("Request Payload :: "+createDaySchedule);
+        helperUtil.addStep("Request Payload :: "+featuredPosts);
 
         fetch(JSONData.AutoTextList[0].BASE_URL + JSONData.AutoTextList[0].REDIRECT_URL, {
             method: 'POST',
             headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + global.authToken},
-            body: JSON.stringify({query: createDaySchedule}),
+            body: JSON.stringify({query: featuredPosts}),
         }).then(function (res) {
 
             return res.json();
@@ -89,14 +88,14 @@ describe('Test GraphQL SCHEDULE API queries', function () {
         });
     });
 
-    it('ZESTY_SCHEDULE-004 :Create Weekly Schedule api', function (done) {
+    it('ZESTY_POST-004 :Get Post By ID api', function (done) {
 
-        helperUtil.addStep("Request Payload :: "+createWeeklySchedule);
+        helperUtil.addStep("Request Payload :: "+getPostById);
 
         fetch(JSONData.AutoTextList[0].BASE_URL + JSONData.AutoTextList[0].REDIRECT_URL, {
             method: 'POST',
             headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + global.authToken},
-            body: JSON.stringify({query: createWeeklySchedule}),
+            body: JSON.stringify({query: getPostById}),
         }).then(function (res) {
 
             return res.json();
@@ -107,14 +106,14 @@ describe('Test GraphQL SCHEDULE API queries', function () {
         });
     });
 
-    it('ZESTY_SCHEDULE-005 :Update Day Schedule api', function (done) {
+    it('ZESTY_POST-005 :Posts api', function (done) {
 
-        helperUtil.addStep("Request Payload :: "+updateDaySchedule);
+        helperUtil.addStep("Request Payload :: "+posts);
 
         fetch(JSONData.AutoTextList[0].BASE_URL + JSONData.AutoTextList[0].REDIRECT_URL, {
             method: 'POST',
             headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + global.authToken},
-            body: JSON.stringify({query: updateDaySchedule}),
+            body: JSON.stringify({query: posts}),
         }).then(function (res) {
 
             return res.json();
@@ -125,14 +124,14 @@ describe('Test GraphQL SCHEDULE API queries', function () {
         });
     });
 
-    it('ZESTY_SCHEDULE-006 :Update Weekly Schedule api', function (done) {
+    it('ZESTY_POST-006 :Update Post api', function (done) {
 
-        helperUtil.addStep("Request Payload :: "+updateWeeklySchedule);
+        helperUtil.addStep("Request Payload :: "+updatePost);
 
         fetch(JSONData.AutoTextList[0].BASE_URL + JSONData.AutoTextList[0].REDIRECT_URL, {
             method: 'POST',
             headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + global.authToken},
-            body: JSON.stringify({query: updateWeeklySchedule}),
+            body: JSON.stringify({query: updatePost}),
         }).then(function (res) {
 
             return res.json();
